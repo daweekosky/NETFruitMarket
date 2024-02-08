dd8df1a969898f2e3e74ce0ac2587328d6959431
daweekosky <dawid_czarnecki@o2.pl>
2024-02-08 20:47:43 +0100

    Added LINQ sorting for List of Fruits

--- Program.cs
+++ Program.cs
@@ -7,7 +7,15 @@ for(int i = 0; i < 15; i++)
     fruits.Add(Fruit.Create());
 }
 
+Console.WriteLine("Fruits:");
 foreach(var fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+var fruitsSorted = fruits.Where(x => x.IsSweet == true).OrderByDescending(x => x.Price);
+Console.WriteLine("Sorted fruits:");
+foreach (var fruit in fruitsSorted)
 {
     Console.WriteLine(fruit);
 }
