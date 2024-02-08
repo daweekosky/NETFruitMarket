a4b98219ca3a26e27448832221d85fcf4faa4f08
daweekosky <dawid_czarnecki@o2.pl>
2024-02-08 20:17:22 +0100

    Created project, Fruit class with static Create and ToStrong override

---
+++ Fruit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitMarket
+{
+    internal class Fruit
+    {
+        public string Name {  get; set; }
+        public bool IsSweet {  get; set; }
+        public double Price { get; set; }
+
+        public static Fruit Create()
+        {
+            Random r = new Random();
+            string[] names = new string[] { "Apple", "Banana", "Cherry", "Durian", "Edelberry", "Grape", "Jackfruit" };
+
+            return new Fruit
+            {
+                Name = names[r.Next(names.Length)],
+                IsSweet = r.NextDouble() > 0.5,
+                Price = r.NextDouble() * 10
+            };
+        }
+
+        public override string ToString()
+        {
+            return "Fruit: Name=" + Name + ", IsSweet=" + IsSweet + ", Price=" + Price.ToString("C2");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using FruitMarket;
+
+List<Fruit> fruits = new List<Fruit>();
+
+for(int i = 0; i < 15; i++)
+{
+    fruits.Add(Fruit.Create());
+}
+
+foreach(var fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
